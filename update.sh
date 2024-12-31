b98c7238022dad41da1bbc10fc2aca6e96638d16
Barton Phillips <bartonphillips@gmail.com>
2024-12-31 18:34:25 +0000

    Added update.sh which does an mk-html.sh, a git add and commit and then runs two git log s to update the simple-gitlog in bartonlp.com and the limited-gitlog in this directory. Added limited-gitlog to .gitignore. Moved update-gitlog.sh to Archive. 	modified:   .gitignore   renamed:    mk-html-gitlog.sh -> mk-html.sh 	deleted:    update-git-log.sh 	new file:   update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+./mk-html.sh
+git add .
+git commit
+git log --all --graph -p --decorate > ~/www/bartonlp.com/simple-gitlog
+git log --all --graph -p --decorate -2 > ./limited-gitlog
+echo "Run 'git push' when ready"
